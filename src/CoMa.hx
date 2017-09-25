1f7015e6d8392c17092e6802060d2db4b75d63b1
Yann <ga29qal@mytum.de>
2017-09-25 08:21:06 +0200

    first steps towards CoMa

---
+++ src/CoMa.hx
@@ -0,0 +1,12 @@
+import parsing.LstParser;
+import util.Pair;
+
+class CoMa {
+    public static function createTable(l:List<List<Pair<String, String>>):String {
+        
+    }
+
+    public static function main():Void {
+
+    }
+}
---
+++ src/parsing/LstParser.hx
@@ -0,0 +1,30 @@
+package parsing;
+
+import util.Pair;
+
+class LstParser {
+    public static function parseLst(fileContent:String):List<Pair<String, String>> {
+        var result:List<Pair<String, String>> = new List<Pair<String, String>>();
+        var lines:Array<String> = fileContent.split("\n");
+        var lineNo:Int = 0;
+        for(line in lines) {
+            lineNo++;
+            if(line == null || line == "" || line.charAt(0) == "#") {
+                continue;
+            }
+            var pos:Int = line.lastIndexOf("\t");
+            if(pos == -1) {
+                throw "Missing tab character in line " + lineNo;
+            }
+            var name:String = line.substring(0, pos);
+            name = Parse.stripString(name);
+            var chr:String = line.substring(pos + 1);
+            chr = Parse.stripString(chr);
+            if(name == null || name == "" || chr == null || chr == "") {
+                throw "Error in line " + lineNo;
+            }
+            result.add(new Pair(name, chr));
+        }
+        return result;
+    }
+}
